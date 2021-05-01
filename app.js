e6591eca011d6426861e0d3073ec4471c6c17485
knee-rel <nirelmarieibarra@gmail.com>
2021-05-01 20:30:57 +0800

    configured backeend

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import { notFound, errorHandler } from "./middlewear/errorMiddlewear.js";
+import connectDB from "./config/db.js";
+
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("API is running!");
 });
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
